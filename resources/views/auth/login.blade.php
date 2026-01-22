7e3ce43973d49479c8b08f33234e4444bfdfb285
Gudangsoft <gudangsoft.net@gmail.com>
2026-01-22 21:51:58 +0700

    feat: Open slot journal info link in a new tab on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -139,7 +139,7 @@
 
                             <div class="mt-3 text-center">
                                 <hr class="my-3">
-                                <a href="{{ route('public.slot.info') }}" class="btn btn-info w-100 mb-2">
+                                <a href="{{ route('public.slot.info') }}" target="_blank" class="btn btn-info w-100 mb-2">
                                     <i class="bi bi-calendar-range"></i> Info Slot Jurnal
                                 </a>
                                 <a href="{{ route('reviewer-registration.form') }}" class="btn btn-success w-100 mb-2">
