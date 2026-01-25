1a4571caa42633df0047b4d5e4453f719e2de0ea
Gudangsoft <gudangsoft.net@gmail.com>
2026-01-25 11:25:41 +0700

    Fix pagination to show limited page numbers with ellipsis

--- resources/views/components/simple-pagination.blade.php
+++ resources/views/components/simple-pagination.blade.php
@@ -1,6 +1,23 @@
 @if ($paginator->hasPages())
+    @php
+        $currentPage = $paginator->currentPage();
+        $lastPage = $paginator->lastPage();
+        $showPages = 5; // Number of page links to show
+        $halfShow = floor($showPages / 2);
+        
+        $startPage = max(1, $currentPage - $halfShow);
+        $endPage = min($lastPage, $currentPage + $halfShow);
+        
+        // Adjust if we're near the start or end
+        if ($currentPage <= $halfShow) {
+            $endPage = min($lastPage, $showPages);
+        }
+        if ($currentPage > $lastPage - $halfShow) {
+            $startPage = max(1, $lastPage - $showPages + 1);
+        }
+    @endphp
     <nav aria-label="Page navigation">
-        <ul class="pagination justify-content-center">
+        <ul class="pagination justify-content-center flex-wrap">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled">
@@ -12,9 +29,21 @@
                 </li>
             @endif
 
+            {{-- First Page --}}
+            @if ($startPage > 1)
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->url(1) }}">1</a>
+                </li>
+                @if ($startPage > 2)
+                    <li class="page-item disabled">
+                        <span class="page-link">...</span>
+                    </li>
+                @endif
+            @endif
+
             {{-- Page Numbers --}}
-            @foreach(range(1, $paginator->lastPage()) as $page)
-                @if($page == $paginator->currentPage())
+            @foreach(range($startPage, $endPage) as $page)
+                @if($page == $currentPage)
                     <li class="page-item active">
                         <span class="page-link">{{ $page }}</span>
                     </li>
@@ -24,6 +53,18 @@
                     </li>
                 @endif
             @endforeach
+
+            {{-- Last Page --}}
+            @if ($endPage < $lastPage)
+                @if ($endPage < $lastPage - 1)
+                    <li class="page-item disabled">
+                        <span class="page-link">...</span>
+                    </li>
+                @endif
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->url($lastPage) }}">{{ $lastPage }}</a>
+                </li>
+            @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
