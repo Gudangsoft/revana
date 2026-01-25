98e6030ac52ac0a4ac65dce4324297bffc915218
Gudangsoft <gudangsoft.net@gmail.com>
2026-01-25 14:21:06 +0700

    Add favicon to marketing layout and login page

--- resources/views/marketing/login.blade.php
+++ resources/views/marketing/login.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Marketing - REVANA</title>
+    @if(isset($appSettings['favicon']) && $appSettings['favicon'])
+    <link rel="icon" href="{{ asset('storage/' . $appSettings['favicon']) }}" type="image/x-icon">
+    @endif
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <style>
