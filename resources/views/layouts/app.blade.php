c32010583233318f1e99869dd632aa71c2767f11
Gudangsoft <gudangsoft.net@gmail.com>
2026-01-25 23:32:55 +0700

    fix: Add CSRF token meta tag for enhanced security in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <!-- SEO Meta Tags -->
     <meta name="description" content="SIPERA - Sistem Insentif dan Penghargaan Reviewer APJI. Platform manajemen jurnal ilmiah, review artikel, dan penghargaan bagi reviewer di Indonesia.">
