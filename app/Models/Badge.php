12d2ee3c023b1403f73dd1570d6e11677b74d547
Gudangsoft <gudangsoft.net@gmail.com>
2026-02-06 14:23:50 +0700

    feat: Add Badge model with relationships and attributes

---
+++ app/Models/Badge.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Badge extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'icon',
+        'required_reviews',
+    ];
+
+    protected $casts = [
+        'required_reviews' => 'integer',
+    ];
+
+    /**
+     * Users who have earned this badge
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_badges')
+            ->withTimestamps()
+            ->withPivot('earned_at');
+    }
+}
